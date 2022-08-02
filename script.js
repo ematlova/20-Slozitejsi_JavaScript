671300701a4e79801d8a61de3c9143ceb47164fb
Ester Mátlová <ester.matlova@gmail.com>
2022-08-02 10:12:51 +0200

    zmeny

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("JavaScript funguje!");
+
+let visualniHlavniOdstavec = document.querySelector(".hlavni-odstavec");
+visualniHlavniOdstavec.textContent = "Domovská stránka"; 
+
+let visualniMojeFoto = document.querySelector(".moje-foto"); 
+visualniMojeFoto.src = "img/druhe-foto.jpeg";
+
+let visualniPredstaveniSe = document.querySelector(".predstaveni-se");
+visualniPredstaveniSe.style.color = "darkblue";
+
+let visualniVzdelani = document.querySelector(".vzdelani");
+visualniVzdelani.textContent = "Školy";
+
+function pridejZvyrazneni(){
+    let visualniTlacitko1 = document.querySelector(".tlacitko1");
+    visualniTlacitko1.classList.add ("zvyrazneny");
+}
